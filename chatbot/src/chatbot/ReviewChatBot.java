966481c968981212448de595d173146fcac812b7
Nick <nicholas.gerritsen@hotmail.com>
2020-03-02 16:33:19 -0800

    Second commit.

--- chatbot/src/chatbot/ReviewChatBot.java
+++ chatbot/src/chatbot/ReviewChatBot.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class ReviewChatBot extends JFrame{
 
 	public static void main(String[] args) {
-		System.out.println("This is the first commit!");
+		System.out.println("This is the second commit!");
 	}
 
 }
