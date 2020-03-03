30b88c4db66092ea2372ce3a011c8b09c6155b98
Nick <nicholas.gerritsen@hotmail.com>
2020-03-02 16:20:47 -0800

    First commit for Nicholas

---
+++ chatbot/src/chatbot/ReviewChatBot.java
@@ -0,0 +1,11 @@
+package chatbot;
+
+import javax.swing.*;
+
+public class ReviewChatBot extends JFrame{
+
+	public static void main(String[] args) {
+		System.out.println("This is the first commit!");
+	}
+
+}
